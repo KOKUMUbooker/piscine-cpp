33e67cbe638dba951031817145eeaab08599b452
KOKUMUbooker <bookerochieng20@gmail.com>
2026-07-29 20:58:17 +0300

    Add quadB calls in main

--- raid1_quad/main.cpp
+++ raid1_quad/main.cpp
@@ -13,4 +13,14 @@ int main()
     QuadA(1,1);
     std::cout << "QuadA(1,5)\n";
     QuadA(1,5);
+
+    std::cout << "\n============== QUAD B ===============\n";
+    std::cout << "QuadB(5,3)\n";
+    QuadB(5,3);
+    std::cout << "QuadB(5,1)\n";
+    QuadB(5,1);
+    std::cout << "QuadB(1,1)\n";
+    QuadB(1,1);
+    std::cout << "QuadB(1,5)\n";
+    QuadB(1,5);
 }
