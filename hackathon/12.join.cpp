1b1946c5cc2a89cc020a6648303d1ee743ca044d
KOKUMUbooker <bookerochieng20@gmail.com>
2026-08-07 21:07:42 +0300

    Complete join

---
+++ hackathon/12.join.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+/*
+Write a function that returns the concatenation of all the strings of a slice of strings separated by the separator passed as the argument sep.
+
+Expected function
+func Join(strs []string, sep string) string {
+
+}
+
+Usage (Go) :
+toConcat := []string{"Hello!", " How", " are", " you?"}
+fmt.Println(piscine.Join(toConcat, ":"))
+
+Output : 
+Hello!: How: are: you?
+*/
+
+namespace piscine
+{
+    std::string Join(const std::vector<std::string>& strs, std::string sep)
+    {
+        std::string res {""};
+        int len {static_cast<int>(strs.size())};
+        for (int i {0}; i < len; ++i)
+        {
+            res += strs[i];
+            if (i != (len-1))
+                res += sep;
+        }
+
+        return res;
+    }
+} // namespace piscine
+
+
+int main()
+{
+    std::vector<std::string> toConcat {"Hello!", " How", " are", " you?"};
+    std::cout << piscine::Join(toConcat, ":") << "\n";
+
+    return EXIT_SUCCESS;
+}
