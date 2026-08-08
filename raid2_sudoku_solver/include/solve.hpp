e18bbe5349c19d014f29d72e3335c8be2f7f8e66
KOKUMUbooker <bookerochieng20@gmail.com>
2026-08-08 14:58:13 +0300

    add header guards

--- raid2_sudoku_solver/include/solve.hpp
+++ raid2_sudoku_solver/include/solve.hpp
@@ -1,6 +1,11 @@
+#ifndef _INCLUDE_SOLVE_
+#define _INCLUDE_SOLVE_
+
 #include <string>
 #include <vector>
 
 bool Solve(std::vector<std::string>& grid);
 bool FindUnassignedLocation(const std::vector<std::string>& grid, int& row, int& col);
 bool NoConFlicts(const std::vector<std::string>& grid, int row, int col, char num);
+
+#endif
--- raid2_sudoku_solver/include/utils.hpp
+++ raid2_sudoku_solver/include/utils.hpp
@@ -1,5 +1,9 @@
+#ifndef _INCLUDE_UTILS_
+#define _INCLUDE_UTILS_
+
 #include <vector>
 #include <string>
 
 bool ValidateArgs(const std::vector<std::string>& args);
 void PrintGrid(const std::vector<std::string>& args);
+#endif
